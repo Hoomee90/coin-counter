d1bed45ced2eebfaca0da6d6494d32e1319a818e
Samantha Callie <Hoomee90@gmail.com>
2024-09-27 13:30:03 -0700

    add functions and tests

--- __tests__/script.test.js
+++ __tests__/script.test.js
@@ -1,5 +1,49 @@
-import { coinCount } from "../src/script.js";
+import * as script from "../src/script.js";
 
-test('coinCount exists', () => {
-  expect(coinCount).toBeDefined();
+describe(`toCents`, () => {
+  test('exists', () => {
+    expect(script.toCents).toBeDefined();
+  });
+
+  test('returns int', () => {
+    expect(typeof script.toCents()).toEqual("number");
+  });
+
+  test('toCents converts dollar amounts to cents', () => {
+    expect(script.toCents(4.99)).toEqual(499);
+  });
+});
+
+describe(`getChange`, () => {
+  test('function exists', () => {
+    expect(script.getChange).toBeDefined();
+  });
+
+  test('returns array-like', () => {
+    expect(typeof script.getChange(499)).toEqual("object");
+  });
+
+  test('returns empty object on input of zero', () => {
+    expect(script.getChange(0)).toEqual({});
+  });
+
+  test('returns object with the number of pennies', () => {
+    expect(script.getChange(0.04)).toEqual({ penny: 4 });
+  });
+
+  test('returns object with the number of nickles and pennies', () => {
+    expect(script.getChange(0.08)).toEqual({ nickel: 1, penny: 3 });
+  });
+
+  test('returns object with the amount of quarters', () => {
+    expect(script.getChange(1.25)).toEqual({ quarter: 5 });
+  });
+
+  test('returns object with the full amount of all change', () => {
+    expect(script.getChange(4.99)).toEqual({ quarter: 19, dime: 2, penny: 4 });
+  });
+
+  test('returns successfully on large numbers', () => {
+    expect(script.getChange(4136.12)).toEqual({ quarter: 16544, dime: 1, penny: 2 });
+  });
 });
--- src/script.js
+++ src/script.js
@@ -1,3 +1,26 @@
-export const coinCount = (amount) => {
-  const coinValues = [25, 10, 5, 1];
-}
+export { getChange, toCents, calculateCoins }
+
+const getChange = (amount) => calculateCoins(toCents(amount), {});
+
+const toCents = (amount) => Math.round(amount * 100);
+
+const calculateCoins = (amount, coins) => {
+  const coinValues = {
+    quarter: 25,
+    dime: 10,
+    nickel: 5,
+    penny: 1
+  };
+
+  if (amount === 0) return coins;
+
+  const [coinType, coinValue] = Object.entries(coinValues)
+    .find(([_, value]) => value <= amount);
+
+  const coinCount = Math.floor(amount / coinValue);
+
+  return calculateCoins(
+    amount - (coinCount * coinValue),
+    { ...coins, [coinType]: (coins[coinType] || 0) + coinCount }
+  );
+};
